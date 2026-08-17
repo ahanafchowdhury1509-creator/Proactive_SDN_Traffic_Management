ea942eaf20e12fde2408002f18f7aa5bb4998794
Auditta Chowdhury <ahanafchowdhury1509@gmail.com>
2026-08-17 16:49:22 +0600

    Packet Handler Almost Done

--- Controller/switch.py
+++ Controller/switch.py
@@ -19,3 +19,24 @@ class exsw13(app_manager.RyuApp):
             match=parser.OFPMathch()
             actions=[parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,ofproto.OFPCML_NO_BUFFER)]
             self.add_flow(datapath,0,match,actions)
+     def add_flow(self,datapath,priority,match,actions):
+            ofproto=datapath.ofproto
+            parser=datapath.ofproto_parser
+            inst=[parser.OFPInstructionActions(ofproto.OFFPIT_APPLY_ACTIONS,actions)]
+            mod=parser.OFPFlowMod(datapath=datapath,priority=priority,match=match,instructions=inst)
+            datapath.send_msg(mod)
+
+     @set_ev_cls(ofp_event.EventOFPacketIn,MAIN_DISPATCHER)
+     def _packet_in_handler(self,ev):
+          datapath=ev.msg.datapath
+          ofproto=datapath.ofproto
+          parser=datapath.ofproto_parser
+          in_port=ev.msg.match['in_port']
+          dpid=datapath.id
+          self.mac_to_port.setdefault(dpid,{})
+          pkt=packet.Packet(ev.msg.data)
+          eth=pkt.get_protocols(ethernet.ethernet)[0]
+          dst=eth.dst
+          src=eth.src
+          in_port=ev.msg.match['in_port']
+          self.mac_to_port[dpid][src]=in_port
