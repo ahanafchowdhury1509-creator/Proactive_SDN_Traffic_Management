f778526ea0d06578cc16efb0b5e0147d938f879f
Auditta Chowdhury <ahanafchowdhury1509@gmail.com>
2026-08-26 15:49:05 +0600

    Finish Monitoring

--- Controller/monitor.py
+++ Controller/monitor.py
@@ -36,4 +36,24 @@ class Monitor(switch.exsw13):
      @set_ev_cls(ofp_event.EventOFFlowStatsReply,MAIN_DISPATCHER )
      def flow_stats_reply_handler(self,ev):
            body=ev.msg.body
-            
+           self.logger.info('datapath         ',
+                            'in-port  eth-dst           '
+                            'out-port packets  bytes')
+           for stat in sorted([flow for flow in body if flow.priority==1],
+                              key=lambda flow:(flow.match['in_port'],flow.match['eth_dst'])):
+                self.logger.info('%016x %8x %17s %8x %8d %8d',
+                                 ev.msg.datapath.id,
+                                 stat.match['in_port'],stat.match['eth_dst'],
+                                 stat.instructions[0].actions[0].port,
+                                 stat.packet_count,stat.byte_count)    
+     @set_ev_cls(ofp_event.EventOFPPortStatsReply,MAIN_DISPATCHER)
+     def port_stats_reply_handler(self,ev):
+           body=ev.msg.body                
+           self.logger.info('datapath         port     ',
+                             'rx-pkts  rx-bytes rx-error '
+                            'tx-pkts  tx-bytes tx-error')
+           for stat in sorted(body,key=attrgetter('port_no')):
+                 self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+                                  ev.msg.datapath.id,stat.port_no,
+                                  stat.rx_packets,stat.rx_bytes,stat.rx_errors,
+                                  stat.tx_packets,stat.tx_bytes,stat.tx_errors)
